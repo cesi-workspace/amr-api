f09630d1bae1dbe4e4e67dcb25ee790e9b3bd4f2
Yohan DEBUT <debut.yohan@gmail.com>
2023-05-13 22:58:04 +0200

    Ajout de la migration pour le Chiffrement précédant
    Modifications de structure de la base de données :
    - Chiffrement des données existantes pour les champs 'address_ip' de la table 'Connection' ; 'content' de la table 'Message' (Cette migration doit obligatoirement être exécutée pour que les données de Message et de connexion soit aussi chiffrée comme le seront les nouvelles données)

---
+++ migrations/Version20230513204709.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230513204709 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql("set names 'utf8'; SET block_encryption_mode = 'aes-256-ecb'; SET @cle='840c6a1f520fec9f74199ed85088d476';");
+        $this->addSql('UPDATE connection set ip_address=TO_BASE64(AES_ENCRYPT(ip_address,FROM_BASE64(@cle)))');
+        $this->addSql('UPDATE message set content=TO_BASE64(AES_ENCRYPT(content,FROM_BASE64(@cle)))');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql("set names 'utf8'; SET block_encryption_mode = 'aes-256-ecb'; SET @cle='840c6a1f520fec9f74199ed85088d476';");
+        $this->addSql('UPDATE connection set ip_address=AES_DECRYPT(FROM_BASE64(ip_address), FROM_BASE64(@cle))');
+        $this->addSql('UPDATE message set content=AES_DECRYPT(FROM_BASE64(content), FROM_BASE64(@cle))');
+    }
+}
