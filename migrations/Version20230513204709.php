6d49da3412e339700b5d387f239c97aa6204df5a
Yohan DEBUT <debut.yohan@gmail.com>
2023-05-14 01:37:32 +0200

    Correction Migrations référence vers env

--- migrations/Version20230513204709.php
+++ migrations/Version20230513204709.php
@@ -20,7 +20,7 @@ final class Version20230513204709 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql("set names 'utf8'; SET block_encryption_mode = 'aes-256-ecb'; SET @cle='840c6a1f520fec9f74199ed85088d476';");
+        $this->addSql("set names 'utf8'; SET block_encryption_mode = 'aes-256-ecb'; SET @cle='".$_ENV['APP_SECRET']."';");
         $this->addSql('UPDATE connection set ip_address=TO_BASE64(AES_ENCRYPT(ip_address,FROM_BASE64(@cle)))');
         $this->addSql('UPDATE message set content=TO_BASE64(AES_ENCRYPT(content,FROM_BASE64(@cle)))');
     }
@@ -28,7 +28,7 @@ final class Version20230513204709 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql("set names 'utf8'; SET block_encryption_mode = 'aes-256-ecb'; SET @cle='840c6a1f520fec9f74199ed85088d476';");
+        $this->addSql("set names 'utf8'; SET block_encryption_mode = 'aes-256-ecb'; SET @cle='".$_ENV['APP_SECRET']."';");
         $this->addSql('UPDATE connection set ip_address=AES_DECRYPT(FROM_BASE64(ip_address), FROM_BASE64(@cle))');
         $this->addSql('UPDATE message set content=AES_DECRYPT(FROM_BASE64(content), FROM_BASE64(@cle))');
     }
